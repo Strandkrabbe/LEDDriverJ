3f453539bfceadbdb0628c32446616b6279c2952
Strandkrabbe <strandkrabbe@uniqy.de>
2020-12-20 13:24:57 +0100

    Fixed exports

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module LEDDriverJ {
 	exports henning.leddriverj;
 	exports henning.leddriverj.util;
 	exports henning.leddriverj.input;
+	exports henning.leddriverj.draw;
 	
 	requires transitive java.desktop;
 	requires pi4j.core;
