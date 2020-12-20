33f67210ec3237d43f0a8652e6748c1c7d4acda3
Strandkrabbe <strandkrabbe@uniqy.de>
2020-12-20 14:09:36 +0100

    Updated log

--- src/henning/leddriverj/util/Log.java
+++ src/henning/leddriverj/util/Log.java
@@ -1,5 +1,7 @@
 package henning.leddriverj.util;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -8,103 +10,160 @@ import java.util.Scanner;
 
 public class Log {
 	
-	private static PrintStream out = System.out;
-	private static PrintStream err = System.err;
-	private static boolean debug = true;
-	private static boolean enable = true;
-	private static boolean detailedError = true;
-	public static Scanner Sc = new Scanner(System.in);
+	private static boolean enabled = true;
+	private static PrintStream fileOutput = null;
+	private static int currentLogLevel = LogLevel.INFO.ll;
+	private static Scanner defaultIn = new Scanner(System.in);
+	
+	public static enum LogLevel {
+		TRACE(-20,"TRACE"),DEBUG(-10,"DEBUG"),INFO(0,"INFO"),WARN(10,"WARN"),ERROR(20,"ERROR"),REQUEST(30,"REQUEST");
+		
+		public final String name;
+		public final int ll;
+		private LogLevel(int i,String name)	{
+			this.ll = i;
+			this.name = name;
+		}
+	}
+	public static interface SimpleLog	{
+		public default void log(LogLevel l,String s)	{
+			Log.log(l, s, this.getClass().getSimpleName());
+		}
+		public default void info(String s)	{
+			Log.log(LogLevel.INFO, s, this.getClass().getSimpleName());
+		}
+		public default void warn(String s)	{
+			Log.log(LogLevel.WARN,s,this.getClass().getSimpleName());
+		}
+		public default void error(String s)	{
+			Log.log(LogLevel.ERROR, s,this.getClass().getSimpleName());
+		}
+		public default void debug(String s)	{
+			Log.log(LogLevel.DEBUG, s,this.getClass().getSimpleName());
+		}
+		public default void error(Exception e)	{
+			Log.log(LogLevel.ERROR, e.getClass().getName() + ": " + e.getMessage(),this.getClass().getSimpleName());
+			e.printStackTrace(System.err);
+			if (fileOutput != null)
+				e.printStackTrace(fileOutput);
+		}
+	}
 	
 	public static boolean isDebugEnabled()	{
-		return debug;
+		return currentLogLevel <= LogLevel.DEBUG.ll;
+	}
+	public static void setDebug(boolean enabled)	{
+		if (enabled)
+			currentLogLevel = LogLevel.DEBUG.ll;
+		else
+			currentLogLevel = LogLevel.INFO.ll;
+	}
+	public static void setLogLevel(int ll)	{
+		currentLogLevel = ll;
+	}
+	public static void setLogLevel(LogLevel l)	{
+		currentLogLevel = l.ll;
+	}
+	public void setEnabled(boolean enabled)	{
+		Log.enabled = enabled;
+	}
+	public static void setFileOutput(File f) throws IOException	{
+		if (fileOutput != null)	{
+			fileOutput.close();
+		}
+		if (!f.exists())
+			f.createNewFile();
+		if (f.isFile())	{
+			Log.fileOutput = new PrintStream(f);
+		}
 	}
 	
-	public static void setOutputStream(PrintStream output)	{
-		out = output;
+	public static void println(String s)	{
+		System.out.println(s);
+		if (fileOutput != null)
+			fileOutput.println(s);
 	}
-	public static void setErrorStream(PrintStream error)	{
-		err = error;
+	public static void print(String s)	{
+		System.out.print(s);
+		if (fileOutput != null)
+			fileOutput.println(s);	// Print line since user input is not shown here
 	}
-	public static void setDebug(boolean u)	{
-		debug = u;
+	public static void printErrLn(String s)	{
+		System.err.println(s);
+		if (fileOutput != null)
+			fileOutput.println("E:" + s);
 	}
-	public static void setEnabled(boolean e)	{
-		enable = e;
-	}
-	public static void setDetailedErrors(boolean d)	{
-		detailedError = d;
-	}
-	public static String request(String v)	{
-		if (enable)
-			out.print(getTimeString() + " [Request] " + v + ": ");
-		return Log.Sc.nextLine();
-	}
-	public static String request(String v,Scanner Sc)	{
-		if (enable)
-			out.print(getTimeString() + " [Request] " + v + ": ");
-		return Sc.nextLine();
-	}
-	public static String importantRequest(String v)	{
-		err.print(getTimeString() + " [Request] " + v + ": ");
-		return Log.Sc.nextLine();
-	}
-	public static void out(String v)	{
-		if (enable)
-			out.println(getTimeString() + " [Info] " + v);
-	}
-	public static void out(String v,String caller)	{
-		if (enable)
-			out.println(getTimeString() + " [Info] [" + caller + "] " + v);
-	}
-	public static void info(String v)	{
-		out(v);
-	}
-	public static void info(String v,String caller)	{
-		out(v, caller);
-	}
-	public static void warn(String v)	{
-		if (enable)
-			out.println(getTimeString() + " [Warn] " + v);
-	}
-	public static void warn(String v,String c)	{
-		if (enable)
-			out.println(getTimeString() + " [Warn] [" + c + "] " + v);
-	}
-	public static void error(String v)	{
-		if (enable)	{
-			err.println(getTimeString() + " [ERROR] " + v);
-		}
-	}
-	public static void error(String v,String caller)	{
-		if (enable)	{
-			err.println(getTimeString() + " [ERROR] [" + caller + "] " + v);
-		}
-	}
-	public static void error(Exception e)	{
-		if (enable)	{
-			if (!detailedError)
-				err.println(getTimeString() + " [ERROR] " + e.getClass().getName() + " : " + e.getMessage());
-			else
-				e.printStackTrace(err);
-		}
-	}
-	public static void debug(String v)	{
-		if (debug && enable) out.println(getTimeString() + " [debug] " + v);
-	}
-	public static void debug(String v,String caller)	{
-		if (debug && enable) out.println(getTimeString() + " [debug] [" + caller + "] " + v);
-	}
+	
 	private static String getTimeString()	{
 		Date now = Calendar.getInstance().getTime();
 		return "[" + new SimpleDateFormat("HH:mm:ss:SSS").format(now) + "]";
 	}
-	public static void debug(Exception e)	{
-		if (debug && enable)
-			out.println(getTimeString() + " [debug] " + e.getClass().getName() + " : " + e.getMessage());
+	
+	private static String getPref(LogLevel l,String tag)	{
+		if (tag == null)
+			return getTimeString() + " [" + l.name +  "@" + Thread.currentThread().getName() + "] ";
+		else
+			return getTimeString() + " [" + l.name +  "@" + Thread.currentThread().getName() + "] [" + tag + "] ";
 	}
 	
-	public static void important(String msg,String caller)	{
-		err.println(getTimeString() + " [!!!] [" + caller + "] " + msg);
+	public static void log(LogLevel l,String msg,String tag)	{
+		if (enabled && l.ll >= currentLogLevel)	{
+			if (l == Log.LogLevel.REQUEST) {
+				print(getPref(l, tag) + msg + ": ");
+			} else if (l.ll >= LogLevel.ERROR.ll)	{
+				printErrLn(getPref(l, tag) + msg);
+			} else	{
+				println(getPref(l, tag) + msg);
+			}
+		}
+	}
+	public static void log(LogLevel l,String msg)	{
+		log(l, msg, null);
+	}
+	
+	public static void info(String s)	{
+		log(LogLevel.INFO,s);
+	}
+	public static void info(String s,String tag)	{
+		log(LogLevel.INFO,s,tag);
+	}
+	public static void warn(String s)	{
+		log(LogLevel.WARN,s);
+	}
+	public static void warn(String s,String tag)	{
+		log(LogLevel.WARN,s,tag);
+	}
+	public static void error(String s)	{
+		log(LogLevel.ERROR,s);
+	}
+	public static void error(String s,String tag)	{
+		log(LogLevel.ERROR,s,tag);
+	}
+	public static void error(Exception e)	{
+		log(LogLevel.ERROR,e.getClass().getName() + ": " + e.getMessage());
+		e.printStackTrace(System.err);
+		if (fileOutput != null)
+			e.printStackTrace(fileOutput);
+	}
+	public static void debug(Exception e)	{
+		log(LogLevel.DEBUG,e.getClass().getName() + ": " + e.getMessage());
+		e.printStackTrace(System.out);
+		if (fileOutput != null)
+			e.printStackTrace(fileOutput);
+	}
+	public static void debug(String s)	{
+		log(LogLevel.DEBUG,s);
+	}
+	public static void debug(String s,String tag)	{
+		log(LogLevel.DEBUG,s,tag);
+	}
+	public static String request(String s,String tag)	{
+		log(LogLevel.REQUEST,s,tag);
+		return defaultIn.nextLine();
+	}
+	public static String request(String s)	{
+		log(LogLevel.REQUEST,s);
+		return defaultIn.nextLine();
 	}
 	
 }
